a1f8e0ce0cc40f272f73e70befbd0d1e65a27772
tonmoy <tonmoybasak20567@gmail.com>
2026-02-26 01:00:58 +0600

    filtering use by meal,price and avaiibility

--- src/modules/meals/meals.controller.ts
+++ src/modules/meals/meals.controller.ts
@@ -1,23 +1,42 @@
 import { RequestHandler } from "express"
 import { mealsService } from "./meals.service"
 
-const createMeals:RequestHandler=async(req,res)=>{
+const createMeals: RequestHandler = async (req, res) => {
 
-        try {
-            const result= await mealsService.createMeals(req.body)
-            res.status(201).json(result)
-        } catch (error) {
-            res.status(400).json(error)
-        }
+    try {
+        const result = await mealsService.createMeals(req.body)
+        res.status(201).json(result)
+    } catch (error) {
+        res.status(400).json(error)
+    }
 }
 
-const filterIngMeals:RequestHandler=async(req,res)=>{
-    
+const filterIngMeals: RequestHandler = async (req, res) => {
+    const { search, isAvailable ,maxprice,minprice} = req.query
+    console.log(minprice,maxprice);
+
+
+    try {
+        const searchString = typeof search === "string" ? search : ""
+        const maxPriceConversion= typeof maxprice==="string"?Number(maxprice):undefined
+        const minPriceConversion= typeof minprice==="string"? Number(minprice):undefined
+        console.log(maxPriceConversion,minPriceConversion,"conversion");
+        // console.log(searchString);
+
+        const isAvailableBoolean = isAvailable === "true" ? true : isAvailable === "false" ? false : undefined
+        
+        const result = await mealsService.filterIngMeals({searchString, isAvailableBoolean,maxPriceConversion,minPriceConversion })
+        res.status(200).json(result)
+
+    } catch (error) {
+
+    }
+
 
 }
 
 
-export const mealsController={
+export const mealsController = {
     createMeals,
     filterIngMeals
 }
--- src/modules/meals/meals.service.ts
+++ src/modules/meals/meals.service.ts
@@ -1,20 +1,97 @@
+import { MealWhereInput } from "../../../prisma/generated/prisma/models"
 import { prisma } from "../../lib/prisma"
 
-const createMeals=async(payload:any)=>{
-// payload type can change
-    const mealsCreationByProvider= await prisma.meal.create({
-        data:payload
+const createMeals = async (payload: any) => {
+    // payload type can change
+    const mealsCreationByProvider = await prisma.meal.create({
+        data: payload
     })
     return mealsCreationByProvider
+
+}
+
+
+const filterIngMeals = async ({ searchString, isAvailableBoolean,minPriceConversion,maxPriceConversion }:
+    { searchString: string | "", isAvailableBoolean: boolean | undefined ,minPriceConversion:number | undefined,maxPriceConversion:number |undefined}) => {
+console.log(minPriceConversion,"min");
+// console.log(maxPriceConversion);
+    let andConditions:MealWhereInput[]=[]
+    // console.log(searchString);
+    if(searchString){
+        andConditions.push({
+            OR:[
+                {
+                    mealName:{
+                        contains:searchString,
+                        mode:"insensitive"
+                    }
+                    
+                    
+                }
+            ]
+        })
+    }
+
+
+    if(typeof isAvailableBoolean==="boolean"){
+        andConditions.push(
+            {
+                OR:[
+                    {
+                        isAvailable:isAvailableBoolean
+                    }
+                ]
+            }
+        )
+    }
     
+
+if( minPriceConversion !==undefined &&!isNaN(minPriceConversion)){
+    andConditions.push(
+        {
+                    price:{
+                        gte:minPriceConversion
+                    }
+                
+                
+            
+        }
+    )
+}
+
+if(maxPriceConversion!==undefined &&!isNaN(maxPriceConversion)){
+    andConditions.push(
+        {
+            
+                
+                    price:{
+                        lte:maxPriceConversion
+                    }
+                
+                
+            
+        }
+    )
+}
+
+const result= await prisma.meal.findMany(
+        {
+            where:{
+                AND:andConditions,
+                
+            }
+        }
+    )
+    // console.log(result);
+    return result
+
+
+
 }
 
 
-const filterIngMeals=async(payload:any)=>{
 
-}
-
-export const mealsService={
-createMeals,
-filterIngMeals
+export const mealsService = {
+    createMeals,
+    filterIngMeals
 }
